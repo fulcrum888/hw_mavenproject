17430d164847fb693e075b708c4b9ceff655ab95
fulcrum888 <yaroslav.buneev@gmail.com>
2020-09-24 21:06:50 +0400

    first commit

---
+++ src/main/java/com/buneev/App.java
@@ -0,0 +1,13 @@
+package com.buneev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
